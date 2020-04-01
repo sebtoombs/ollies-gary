d037d2452fa53ce9d306cbd9b831771030305203
Seb Toombs <sebtoombs@gmail.com>
2020-04-02 10:27:13 +1100

    Remove sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ollies' Gary`,
+    description: `Parse a string`,
+    author: `@sebtoombs`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -17,7 +17,7 @@ module.exports = {
     },*/
     //`gatsby-transformer-sharp`,
     //`gatsby-plugin-sharp`,
-    {
+    /*{
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -28,7 +28,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    },*/
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
