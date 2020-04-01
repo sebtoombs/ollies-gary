21823d81aee5424be87fe74113a23f8c54621c0f
Seb Toombs <sebtoombs@gmail.com>
2020-04-02 09:29:11 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,15 +8,15 @@ module.exports = {
     `gatsby-plugin-postcss`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    {
+    /*{
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    },*/
+    //`gatsby-transformer-sharp`,
+    //`gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
